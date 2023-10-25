69c31861b8b2602796a6b7ebecdac5fad06593a4
Akylbek Bolsunbekov <bolsunbekov.akylbek@gmail.com>
2023-10-25 14:42:04 +0600

    Models change

--- book/models.py
+++ book/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Book(models.Model):
     ACTUALITY = (
         ('Actual', 'Actual'),
@@ -16,3 +17,17 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+#
+# class ReviewProgLang(models.Model):
+#     STARS = (
+#         ('*', '*'),
+#         ('**', '**'),
+#         ('***', '***'),
+#         ('****', '****'),
+#         ('*****', '*****')
+#     )
+#     title_lang = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='review_object')
+#     text_review = models.TextField()
+#     rate_stars = models.CharField(max_length=100, choices=STARS)
+#
